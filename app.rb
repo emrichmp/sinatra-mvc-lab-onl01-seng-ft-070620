5bf71195ec0776d738540bb67e35d7c74a765364
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-28 23:18:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,11 +5,4 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/piglatinize' do
-    user_word = params["user_phrase"]
-    word = PigLatinizer.new
-    @pig_latinized_word = word.to_pig_latin(user_word)
-
-    erb :pig_latin
-  end
 end
